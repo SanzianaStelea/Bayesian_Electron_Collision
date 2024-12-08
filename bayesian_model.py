68333c2ac149fe2f996a3ffe07b367651fbab130
SanzianaStelea <karinasanziana1@gmail.com>
2024-12-08 17:56:36 +0100

    Updated function file

--- bayesian_model.py
+++ bayesian_model.py
@@ -1,11 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import gammaln
+
+#------------------------------------- MODEL FUNCTIONS ----------------------------------
 
 def model_gauss_exp(x, a1, mu1, sigma1, a2, mu2, sigma2, b, k):
     gaussian1 = a1 * np.exp(-((x - mu1) ** 2) / (2 * sigma1 ** 2))
     gaussian2 = a2 * np.exp(-((x - mu2) ** 2) / (2 * sigma2 ** 2))
     background = b * np.exp(-k * x)
     return gaussian1 + gaussian2 + background 
+
+def model_single_gauss_exp(x, a1, mu1, sigma1, b, k):
+    gaussian = a1 * np.exp(-((x - mu1) ** 2) / (2 * sigma1 ** 2))
+    background = b * np.exp(-k * x)
+    return gaussian + background
+
+#------------------------------------- LIKELIHOOD FUNCTIONS ----------------------------------
 
 def log_likelihood_probability(x, y, sigma_y, model, params): 
     prediction = model(x, *params)
@@ -15,6 +25,27 @@ def log_likelihood_probability(x, y, sigma_y, model, params):
         -0.5 * np.sum((y - prediction)**2/sigma_y**2)  # Exponent
         - n/2*np.log(2*np.pi*sigma_y**2)               # Normalisation
     )
+    
+def log_likelihood_heteroscedastic(x, y,sigma_y, model, params):
+    prediction = model(x, *params)
+
+    return (
+        -0.5 * np.sum((y - prediction)**2/sigma_y**2)    # Exponent
+        -0.5 * np.sum(np.log(2*np.pi*sigma_y**2))        # Normalisation
+    )
+    
+def log_likelihood_poisson(x, y, model, params):
+    # Model predictions (λ values)
+    lambda_model = model(x, *params)
+    
+    # Log-likelihood computation
+    # Safeguard against taking log of zero or negative λ
+    lambda_model = np.maximum(lambda_model, 1e-10)
+    log_likelihood_value = np.sum(y * np.log(lambda_model) - lambda_model - gammaln(y + 1))
+    
+    return log_likelihood_value
+
+#------------------------------------- PRIOR FUNCTIONS ----------------------------------
     
 def log_prior_probability(params, priors):
     prob = 0
@@ -30,18 +61,46 @@ def sample_prior(n_sample, priors):
     
     return np.array([prior.rvs(n_sample) for prior in priors]).T
 
-def log_posterior_probability(params, x, y, sigma_y, model, priors):
-    return (
-        log_likelihood_probability(x, y, sigma_y, model, params)
-        + log_prior_probability(params, priors)
-    )
+#------------------------------------- POSTERIOR FUNCTIONS ----------------------------------
+
+def log_posterior_probability(params, x, y, sigma_y, model, priors, mode='gaussian'):
     
-def negative_log_posterior(params, x, y, sigma_y, model, priors):
-    return -log_posterior_probability(params, x, y, sigma_y, model, priors)
+    if mode == 'gaussian':
+        return (
+            log_likelihood_probability(x, y, sigma_y, model, params)
+            + log_prior_probability(params, priors)
+        )
+        
+    elif mode == 'poisson':
+        return (
+            log_likelihood_poisson(x, y, model, params)
+            + log_prior_probability(params, priors)
+        )
+        
+    elif mode == 'gaussian heteroscedastic':
+        return (
+            log_likelihood_heteroscedastic(x, y, sigma_y, model, params)
+            + log_prior_probability(params, priors)
+        )
+    
+def negative_log_posterior(params, x, y, sigma_y, model, priors, mode='gaussian'):
+    return -log_posterior_probability(params, x, y, sigma_y, model, priors, mode)
+
+#############################################################################################
+
+def log_posterior_heteroscedastic(params, x, y, sigma_y, model, priors):
+    return log_likelihood_heteroscedastic(x, y,sigma_y, model, params) + log_prior_probability(params, priors)
+
+def negative_log_posterior_heteroscedastic(params, x, y, sigma_y, model, priors):
+    return -log_posterior_heteroscedastic(params, x, y, sigma_y, model, priors)
+
+#############################################################################################
 
 def sample_posterior(x, y, chain, model, seed=15, n_samples=200, plot=True):
+    
+    np.random.seed(seed)
     flat_chain = chain.reshape(-1, chain.shape[-1])
-    chain_samples = flat_chain[np.random.choice(chain.shape[0], size=200)]
+    chain_samples = flat_chain[np.random.choice(chain.shape[0], size=n_samples)]
 
     # Evaluate the model at the sample parameters
     model_predictive = np.array(
@@ -51,13 +110,13 @@ def sample_posterior(x, y, chain, model, seed=15, n_samples=200, plot=True):
         model_predictive, q=[0.025, 0.16, 0.84, 0.975], axis=0
     )
     if plot == True:
-        np.grid()
+        plt.grid()
         plt.fill_between(x, model_quantiles[0], model_quantiles[-1], alpha=0.5, facecolor="C1",
                     label="Model predictive distribution")
         plt.fill_between(x, model_quantiles[1], model_quantiles[-2], alpha=0.5, facecolor="C1")
         plt.plot(x, y, ".", color = 'black')
         
-    return model_predictive
+    return chain_samples, model_predictive
 
 def process_chain(chain, discard=0, thin=1, flat=False):
     """
